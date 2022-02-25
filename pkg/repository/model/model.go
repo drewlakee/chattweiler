c6cf1a7abe689acfa25ab334a77d21fb234c3409
Andrew Aleynikov <drew.lake@yandex.ru>
2022-02-25 21:39:21 +0300

    Added roulette for phrase picking

--- pkg/repository/model/model.go
+++ pkg/repository/model/model.go
@@ -1,25 +1,19 @@
 package model
 
 import (
+	"chattweiler/pkg/repository/model/types"
 	"database/sql"
 	"time"
 )
 
-type PhraseType string
-
-const (
-	Welcome PhraseType = "welcome"
-	Goodbye PhraseType = "goodbye"
-)
-
 type Phrase struct {
-	PhraseID             int            `db:"phrase_id"`
-	Weight               int            `db:"weight"`
-	PhraseType           PhraseType     `db:"phrase_type"`
-	IsUserTemplated      bool           `db:"is_user_templated"`
-	IsAudioAccompaniment bool           `db:"is_audio_accompaniment"`
-	VkAudioId            sql.NullString `db:"vk_audio_id"`
-	Text                 string         `db:"text"`
+	PhraseID             int              `db:"phrase_id"`
+	Weight               int              `db:"weight"`
+	PhraseType           types.PhraseType `db:"phrase_type"`
+	IsUserTemplated      bool             `db:"is_user_templated"`
+	IsAudioAccompaniment bool             `db:"is_audio_accompaniment"`
+	VkAudioId            sql.NullString   `db:"vk_audio_id"`
+	Text                 string           `db:"text"`
 }
 
 type MembershipWarning struct {
---
+++ pkg/repository/model/types/types.go
@@ -0,0 +1,8 @@
+package types
+
+type PhraseType string
+
+const (
+	Welcome PhraseType = "welcome"
+	Goodbye PhraseType = "goodbye"
+)
