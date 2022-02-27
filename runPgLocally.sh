b477ca77027de53eec70e6373cf1a80e6af5ecbb
Andrew Aleynikov <drew.lake@yandex.ru>
2022-02-27 11:26:58 +0300

    Updated pg script

--- runPgLocally.sh
+++ runPgLocally.sh
@@ -10,7 +10,23 @@ if [[ "$OSTYPE" == "linux"* ]]; then
   -e POSTGRES_PASSWORD=${2:-postgres} \
   -d postgres
 elif [[ "$OSTYPE" == "win"* || "$OS" == "Windows"* ]]; then
-  MOUNT_SCRIPT="C:\Users\\$USERNAME\go\src\chattweiler\sql\initdb.sql"
+  MOUNT_SCRIPT="$(pwd)/sql/initdb.sql"
+  MOUNT_SCRIPT=$(echo ${MOUNT_SCRIPT////\\})
+  if [[ "$MOUNT_SCRIPT" == "\c"* ]]; then
+      MOUNT_SCRIPT=$(echo ${MOUNT_SCRIPT/\\c/C:})
+  elif [[ "$MOUNT_SCRIPT" == "\a"* ]]; then
+      MOUNT_SCRIPT=$(echo ${MOUNT_SCRIPT/\a/A:})
+  elif [[ "$MOUNT_SCRIPT" == "\b"* ]]; then
+      MOUNT_SCRIPT=$(echo ${MOUNT_SCRIPT/\b/B:})
+  elif [[ "$MOUNT_SCRIPT" == "\d"* ]]; then
+      MOUNT_SCRIPT=$(echo ${MOUNT_SCRIPT/\d/D:})
+  elif [[ "$MOUNT_SCRIPT" == "\f"* ]]; then
+      MOUNT_SCRIPT=$(echo ${MOUNT_SCRIPT/\f/F:})
+  else
+    echo "runPgLocally.sh: Ooops, I don't know the name of your disk. Please, add me here to script and try again!"
+    return
+  fi
+
   docker run -p 5433:5432 \
   -v "$MOUNT_SCRIPT":/docker-entrypoint-initdb.d/initdb.sql \
   --name postgres-chattweiler \
